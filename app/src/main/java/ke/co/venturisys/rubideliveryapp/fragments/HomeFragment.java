a66458dbf8bddc40b779ab078ea79b14085fe1af
Victor Kamau <victormungai97@gmail.com>
2018-03-20 10:43:29 +0300

    Landing Page

--- app/src/main/java/ke/co/venturisys/rubideliveryapp/fragments/HomeFragment.java
+++ app/src/main/java/ke/co/venturisys/rubideliveryapp/fragments/HomeFragment.java
@@ -15,7 +15,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;

904dc93f082a3052aafafb095b2ee20ae81d1c5b
Victor Kamau <victormungai97@gmail.com>
2018-05-05 13:05:14 +0300

    Connection to backend

--- app/src/main/java/ke/co/venturisys/rubideliveryapp/others/URLs.java
+++ app/src/main/java/ke/co/venturisys/rubideliveryapp/others/URLs.java
@@ -10,7 +10,8 @@ public class URLs {
     public static final String urlTermsConditions = "https://api.androidhive.info/webview/index.html";
     public static final String urlAboutUs = "https://www.youtube.com/watch?v=ua2CYbr9wGQ";
     public static final String urlVenturi = "www.venturisys.co.ke";
-    public static String BASE_URL = "http://192.168.43.229:8000/";
+    // public static String BASE_URL = "http://192.168.43.229:8000/";
+    public static String BASE_URL = "http://victormungai.pythonanywhere.com/";
     public static String MEDIA_URL = BASE_URL + "media/";
     static final String GRAPH_QL_BASE_URL = BASE_URL + "rubi_api/";
 
